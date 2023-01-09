a404a7f1704d486f69dbed3db29a7aae270d67ef
Carlos Haro <carlosharo8530@gmail.com>
2023-01-09 17:45:57 -0600

    Commit Inicial
    En la rama master
    Cambios a ser confirmados:
    	modificados:     src/app/app-routing.module.ts
    	modificados:     src/app/app.component.html
    	modificados:     src/app/app.module.ts
    	nuevos archivos: src/app/models/usuario.model.ts
    	nuevos archivos: src/app/services/usuario.service.spec.ts
    	nuevos archivos: src/app/services/usuario.service.ts
    	nuevos archivos: src/app/shared/navbar/navbar.component.html
    	nuevos archivos: src/app/shared/navbar/navbar.component.scss
    	nuevos archivos: src/app/shared/navbar/navbar.component.spec.ts
    	nuevos archivos: src/app/shared/navbar/navbar.component.ts
    	nuevos archivos: src/app/shared/shared.module.ts
    	nuevos archivos: src/app/usuarios/lista/lista.component.html
    	nuevos archivos: src/app/usuarios/lista/lista.component.scss
    	nuevos archivos: src/app/usuarios/lista/lista.component.spec.ts
    	nuevos archivos: src/app/usuarios/lista/lista.component.ts
    	nuevos archivos: src/app/usuarios/usuario/usuario.component.html
    	nuevos archivos: src/app/usuarios/usuario/usuario.component.scss
    	nuevos archivos: src/app/usuarios/usuario/usuario.component.spec.ts
    	nuevos archivos: src/app/usuarios/usuario/usuario.component.ts
    	nuevos archivos: src/app/usuarios/usuarios.module.ts
    	modificados:     src/index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+// Modulos
+import { SharedModule } from './shared/shared.module';
+
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { UsuariosModule } from './usuarios/usuarios.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        SharedModule,
+        UsuariosModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
